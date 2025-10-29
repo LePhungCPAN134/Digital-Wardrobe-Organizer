bf17d704b56fbbd75663a16f272609cad2d5026a
LePhungCPAN134 <phungle601@gmail.com>
2025-10-29 19:16:28 -0400

    Initial setup for Digital Wardrobe Organizer

---
+++ routes/users.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('Get all users');
+});
+
+router.post('/', (req, res) => {
+  res.send('Add a user');
+});
+
+router.delete('/:id', (req, res) => {
+  res.send(`Delete user with ID ${req.params.id}`);
+});
+
+module.exports = router;
